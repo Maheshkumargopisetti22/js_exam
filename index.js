c24c8c65f0325db229c572b60e38cf7e1cd19a4c
Maheshkumargopisetti22 <gopisettimahesh22@gmail.com>
2023-05-02 16:51:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let x = 5;
+let y = 10;
+let max = 15;
+
+for (let i = x; i <= y; i++) {
+    for (let j = x; j <= y; j++) {
+        val = (x ** i - y ** j) / i * j;
+        if (val >= max && max >= 0);
+
+        console.log(x, y);
+    }
+
+}
+
+var perfectSquare = prompt("Enter a number");
+z = Math.sqrt(perfectSquare)
+if (Math.ceil(Math.sqrt(perfectSquare)) == Math.floor(Math.sqrt(perfectSquare))) {
+    console.log(true);
+
+}
+else {
+    console.log(false);
+}
